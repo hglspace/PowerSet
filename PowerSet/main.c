d0c6c2529db4072f677c8cab0c3489212817c299
hgl <hgl199305@163.com>
2018-02-09 10:58:52 +0800

    回溯算法

--- PowerSet/main.c
+++ PowerSet/main.c
@@ -2,14 +2,35 @@
 //  main.c
 //  PowerSet
 //
-//  Created by zyq on 2018/2/8.
-//  Copyright © 2018年 zyq. All rights reserved.
+//  Created by hgl on 2018/2/8.
+//  Copyright © 2018年 hgl. All rights reserved.
 //
 
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+ 
+    char c[3]={'1','2','3'};
+    char c1[3]={'\0'};
+    void powerSet(int i,int n,char c[],char c1[]);
+    powerSet(0, 2, c, c1);
     return 0;
+}
+
+void powerSet(int i,int n,char c[],char c1[]){
+    if(i>n){
+        int j;
+        printf("[\t");
+        for(j=0;j<=n;j++){
+            if(c1[j]!='\0'){
+                printf("%c\t",c1[j]);
+            }
+        }
+        printf("]\n");
+    }else{
+        c1[i]=c[i];
+        powerSet(i+1, n, c, c1);
+        c1[i]='\0';
+        powerSet(i+1, n, c, c1);
+    }
 }
